3cfbcccd570b55044265e26b00e2101ab45f8cf5
xch8888 <771219879@qq.com>
2022-01-19 15:40:46 +0800

    第一次上传

---
+++ src/test/java/com/xch/boots/HomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xch.boots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
